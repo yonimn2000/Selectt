3eef5fa6f601442d59c95b80b30798284559ba99
Yonatan Mankovich <yonimn2000@gmail.com>
2021-02-13 17:50:56 -0500

    Add project files.

---
+++ Selectt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Selectt.Startup))]
+namespace Selectt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
