45f1d3082c585a93be3923a4064cbcd753e0ba99
AndrewMiller510 <71230763+AndrewMiller510@users.noreply.github.com>
2021-04-05 16:18:32 -0400

    Home Page Changes
    Added buttons to the home page and updated the about page

--- Selectt/Controllers/HomeController.cs
+++ Selectt/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace Selectt.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
